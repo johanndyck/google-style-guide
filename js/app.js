86b59b32753c458924d3d91c5c1a7a1891cabdb7
Johann Dyck <contact@johanndyck.com>
2016-11-26 20:10:30 -0800

    More functionality added.
    Change the text of headline and subhead. Change h1, h2, and p
    independantly. change weight, style and capitalization of h1 and h2.

--- js/app.js
+++ js/app.js
@@ -3,24 +3,138 @@ $('ul.switcher a').click(function(e){
     e.preventDefault();
 });
 
-$('a.georgia').click(function(){
-   $('div.target').css("font-family", "Georgia"); 
+$('.switcherH1 a.georgia').click(function(){
+   $('div.target h1').css("font-family", "Georgia"); 
 });
 
-$('a.helvetica').click(function(){
-   $('div.target').css("font-family", "Helvetica"); 
+$('.switcherH1 a.helvetica').click(function(){
+   $('div.target h1').css("font-family", "Helvetica"); 
 });
 
-$('a.lato').click(function(){ 
+$('.switcherH1 a.lato').click(function(){ 
     if( !$("link[href='https://fonts.googleapis.com/css?family=Lato']").length ){
         $('head').append("<link data-fontlink='lato' href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>");
     }
-   $('div.target').css("font-family", "Lato"); 
+   $('div.target h1').css("font-family", "Lato"); 
 });
 
-$('a.playfair').click(function(){ 
+$('.switcherH1 a.playfair').click(function(){ 
     if( !$("link[href='https://fonts.googleapis.com/css?family=Playfair+Display']").length ){
         $('head').append("<link data-fontlink='lato' href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet'>");
     }
-   $('div.target').css("font-family", "Playfair Display"); 
+   $('div.target h1').css("font-family", "Playfair Display"); 
+});
+
+$('.switcherH2 a.georgia').click(function(){
+   $('div.target h2').css("font-family", "Georgia"); 
+});
+
+$('.switcherH2 a.helvetica').click(function(){
+   $('div.target h2').css("font-family", "Helvetica"); 
+});
+
+$('.switcherH2 a.lato').click(function(){ 
+    if( !$("link[href='https://fonts.googleapis.com/css?family=Lato']").length ){
+        $('head').append("<link data-fontlink='lato' href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>");
+    }
+   $('div.target h2').css("font-family", "Lato"); 
+});
+
+$('.switcherH2 a.playfair').click(function(){ 
+    if( !$("link[href='https://fonts.googleapis.com/css?family=Playfair+Display']").length ){
+        $('head').append("<link data-fontlink='lato' href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet'>");
+    }
+   $('div.target h2').css("font-family", "Playfair Display"); 
+});
+
+$('.switcherP a.georgia').click(function(){
+   $('div.target p').css("font-family", "Georgia"); 
+});
+
+$('.switcherP a.helvetica').click(function(){
+   $('div.target p').css("font-family", "Helvetica"); 
+});
+
+$('.switcherP a.lato').click(function(){ 
+    if( !$("link[href='https://fonts.googleapis.com/css?family=Lato']").length ){
+        $('head').append("<link data-fontlink='lato' href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>");
+    }
+   $('div.target p').css("font-family", "Lato"); 
+});
+
+$('.switcherP a.playfair').click(function(){ 
+    if( !$("link[href='https://fonts.googleapis.com/css?family=Playfair+Display']").length ){
+        $('head').append("<link data-fontlink='lato' href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet'>");
+    }
+   $('div.target P').css("font-family", "Playfair Display"); 
+});
+
+$("#headline").keyup(function(){
+    var content = $('#headline').val();
+    $("div.target h1").html(content);
+});
+
+$("#subhead").keyup(function(){
+    var content = $('#subhead').val();
+    $("div.target h2").html(content);
+});
+
+$('.switcherh1 a.weight').click(function(){
+    if( $('div.target h1').hasClass('bold')){
+        $('div.target h1').css("font-weight", "normal").removeClass('bold'); 
+        $(this).html('Bold');
+    } else {
+        $('div.target h1').css("font-weight", "bold").addClass('bold'); 
+        $(this).html('Unbold');
+    }
+});
+
+$('.switcherh1 a.style').click(function(){
+    if( $('div.target h1').hasClass('italic')){
+        $('div.target h1').css("font-style", "normal").removeClass('italic'); 
+        $(this).html('Italicise');
+    } else {
+        $('div.target h1').css("font-style", "italic").addClass('italic'); 
+        $(this).html('Unitalicise');
+    }
+});
+
+$('.switcherh1 a.caps').click(function(){
+    if( $('div.target h1').hasClass('allcaps')){
+        $('div.target h1').css("text-transform", "none").removeClass('allcaps'); 
+        $(this).html('Make All Caps');
+    } else {
+        $('div.target h1').css("text-transform", "uppercase").addClass('allcaps'); 
+        $(this).html('Remove All Caps');
+    }
+});
+
+$('.switcherh2 a.weight').click(function(){
+    if( $('div.target h2').hasClass('bold')){
+        $('div.target h2').css("font-weight", "normal").removeClass('bold'); 
+        $(this).html('Bold');
+    } else {
+        $('div.target h2').css("font-weight", "bold").addClass('bold'); 
+        $(this).html('Unbold');
+    }
+});
+
+$('.switcherh2 a.style').click(function(){
+    if( $('div.target h2').hasClass('italic')){
+        $('div.target h2').css("font-style", "normal").removeClass('italic'); 
+        $(this).html('Italicise');
+    } else {
+        $('div.target h2').css("font-style", "italic").addClass('italic'); 
+        $(this).html('Unitalicise');
+    }
+});
+
+$('.switcherh2 a.caps').click(function(){
+    if( $('div.target h2').hasClass('allcaps')){
+        $('div.target h2').css("text-transform", "none").removeClass('allcaps'); 
+        $(this).html('Make All Caps');
+    } else {
+        $('div.target h2').css("text-transform", "uppercase").addClass('allcaps'); 
+        $(this).html('Remove All Caps');
+    }
 });
